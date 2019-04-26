c692ccc75b0ffadd6d25ca4e0a9669fbaa534fff
tjforeman <tylerforeman92@gmail.com>
2019-04-26 12:07:54 -0400

    post working on both routes with requirements

--- data/action-router.js
+++ data/action-router.js
@@ -4,6 +4,24 @@ const db=require('./helpers/actionModel.js')
 
 const router= express.Router()
 
+router.post('/',(req,res) =>{
+    const newAction=req.body
+    if (newAction.project_id || !newAction.notes || !newAction.description){
+        res.status(400).json({message:"Please provide a valid project id, as well as a  description and notes for the project."})
+    }else if
+        (newAction.description.length>128) {
+         res.status(400).json({message:' your Description must be less than 128 characters'})
+        }else{
+    db
+    .insert(newAction)
+    .then(action=>{
+        res.status(201).json(action)
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The action information could not be modified."})
+    })
+    }
+     })
 
 router.get('/', (req,res)=>{
     db
--- data/project-router.js
+++ data/project-router.js
@@ -4,6 +4,23 @@ const db=require('./helpers/projectModel.js')
 
 const router= express.Router()
 
+router.post('/',(req,res) =>{
+    const newProject=req.body
+    if (!newProject.name && !newProject.description){
+        res.status(400).json({message:"Please provide a name, and description for the project."})
+    }else{
+    db
+    .insert(newProject)
+    .then(post=>{
+        res.status(201).json(post)
+    })
+    .catch(err=>{
+        res.status(500).json({ error: "The project information could not be modified."})
+    })
+    }
+     })
+
+
 router.get('/', (req,res)=>{
     db
     .get()
